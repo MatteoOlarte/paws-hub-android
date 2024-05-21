13a1ac259d8913b6684dda822224b650a225945c
MatteoOlarte <115902090+MatteoOlarte@users.noreply.github.com>
2024-05-21 14:23:14 -0500

    feat: filter dialog styles

--- app/src/main/java/com/software3/paws_hub_android/ui/view/dialogs/FilterDialog.kt
+++ app/src/main/java/com/software3/paws_hub_android/ui/view/dialogs/FilterDialog.kt
@@ -1,22 +1,25 @@
 package com.software3.paws_hub_android.ui.view.dialogs
 
 import android.content.Context
-import android.content.DialogInterface
-import androidx.constraintlayout.motion.widget.MotionScene.Transition.TransitionOnClick
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.software3.paws_hub_android.R
 
 class FilterDialog(private val context: Context) {
     private val dialog = MaterialAlertDialogBuilder(
         context,
-        R.style.ThemeOverlay_PawsHubAndroid_FiltersAlertDialog
+        R.style.ThemeOverlay_PawsHubAndroid_AlertDialog
     )
 
     init {
         dialog.setTitle(context.getString(R.string.apply_filter))
         dialog.setCancelable(true)
-        dialog.setIcon(R.drawable.ic_tune_24)
-
+        dialog.setPositiveButton(context.getString(R.string.filter_dialog_positive)) {i, _ ->
+            i.dismiss()
+        }
+        dialog.setNegativeButton(context.getString(R.string.filter_dialog_negative)) {i, _ ->
+            i.dismiss()
+        }
+        //dialog.setIcon(R.drawable.ic_tune_24)
     }
 
     fun setItems(items: Array<out String>, onClick: (item: Int) -> Unit): FilterDialog {
